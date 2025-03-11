38f1193913604b91707ac9df8da3afab8919161a
Melody <meliaukea@gmail.com>
2025-03-10 21:46:10 -0400

    Put necessary code into server.ts, and added a Weather object and some key components into weatherService.ts

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -5,14 +5,15 @@ import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
-  // TODO: GET weather data from city name
-
-  // TODO: save city to search history
+router.post('/:city', (req, res) => {
+  
+  
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (req, res) => {
+  
+});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req, res) => {});
